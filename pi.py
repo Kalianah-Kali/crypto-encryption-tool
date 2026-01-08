e72ba162e39e7ee6fe87f3b1522057e180ba5731
kalianah <kalianahkali@gmail.com>
2026-01-08 20:11:43 +0100

    home made code

--- pi.py
+++ pi.py
@@ -1,143 +1,86 @@
 #coding:utf-8
 from flask import Flask, render_template, request
-import random
 
-def make_it_first(limit):
-	number = random.randint(2, limit)
-	cp = number - 1
-	while cp >= 2:
-		if number % cp == 0:
-			number = random.randint(2, limit)
-			cp = number - 1
-		else:
-			cp -= 1
-	return number
+#coding:utf-8
+import time
 
-def pgcd(e, phi_n):#with e > phi_n
-	list_ = []
-	try:
-		while e > 0:
-			r = e % phi_n
-			list_ += [r]
-			e = phi_n
-			phi_n = r
-		return list_[len(list_) - 2]
-	except ZeroDivisionError:
-		return list_[len(list_) - 2]
-
-def Extended_Euclid(e, phi_n):
-	#Euclid's descent
-	list_ = []
-	while e % phi_n != 0:
-		list_ += [[e, phi_n, e // phi_n, e % phi_n]]
-		e, phi_n = phi_n, e % phi_n
-
-	if len(list_) < 2:
-		return None
-	if list_[len(list_) - 1][len(list_[len(list_) - 1]) - 1] != 1:
-		return 'the extended Euclid algorithm cannot be applied to these integer!!!'
-	#end of Euclid's descent
-	#=====================================================================
-	#Bezout lift(remontée de Bezout)
-	bezout_table, modulo, counter = [], list_[0][0] % list_[1][0], 0
-	bezout_table = [[list_[0][0], None, 1, 0], [list_[1][0], None, 0, 1]]
-	while modulo != 0:
-		q = bezout_table[counter][0] // bezout_table[counter + 1][0]
-		r = bezout_table[counter][0] % bezout_table[counter + 1][0]
-		bezout_table += [[r, q,
-				bezout_table[counter][2] - q * bezout_table[counter+1][2],
-				bezout_table[counter][3] - q * bezout_table[counter + 1][3]]]
-		counter += 1
-		modulo = bezout_table[counter][0] % bezout_table[counter + 1][0]
-	u, v = bezout_table[len(bezout_table) - 1][2], bezout_table[len(bezout_table) - 1][3]
-	#end of Bezout lift ... With u = d!!!.................................
-	return u
-
-def key_generator():
-	p, q = make_it_first(255), make_it_first(255)
-	n = p * q
-	while p == q or n < 255:
-		p, q = make_it_first(255), make_it_first(255)
-		n = p * q
-	phi_n, e = (p - 1) * (q - 1), make_it_first(255)
-	while pgcd(e, phi_n) != 1:
-		e = make_it_first(255)
-	d = Extended_Euclid(e, phi_n)
-	if d < 0:
-		d = d % phi_n
-	return d, n, e, phi_n
-
-def RSA_Protocol_encryptation(SMS, n, e):
-	string = ''
-	for x in SMS:
-		string += chr((ord(x) ** e) % n)
-	return string
+def afficher(self):
+    for x in self:
+        time.sleep(0.07)
+        print(x, end="")
 
 
-def RSA_Protocol_decryptation(SMS, private_key, public_key):
-	string = ''
-	for x in SMS:
-		string += chr((ord(x) ** private_key) % public_key)
-	return string
+def convertirUnicode(mot):
+    unicode=[]
+    for c in mot:
+        unicode.append(ord(c))
+    return unicode
 
-app = Flask(__name__, template_folder = 'template')#←,static_folder = 'static'
+def dectobin(n):
+    resultat = ""
+    if n < 2:
+        return str(n)
+    while n > 0:
+        resultat = resultat + str(n%2)
+        n = n//2
+    return resultat[::-1]
+
+def convertir_binaire(listeU):
+    listeB=[dectobin(listeU[x]) for x in range(len(listeU))]
+    for x in range(len(listeB)):
+        if len(listeB[x]) != 8:
+            listeB[x] = "0" * (8- len(listeB[x])) + listeB[x]
+    return listeB
+
+def xor(b1,b2):
+    if b1==b2 :
+        return '1'
+    else :
+        return '0'
+
+def retrouver(listeb):
+    ch=""
+    for nb in listeb:
+        ch=ch+chr(int(nb,2))
+    return ch
+
+
+def chiffrerXor(mot,cle):
+    cbcum = convertir_binaire(convertirUnicode(mot))
+    cbcuk = convertir_binaire(convertirUnicode(cle))
+    while len(cbcuk) < len(cbcum):
+        cbcuk += cbcuk
+    cbcuk = cbcuk[0:len(cbcum)]
+    liste = []
+    res = ""
+    for x in range(len(cbcum)):
+        for y in range(8):
+            fi = xor(cbcum[x][y], cbcuk[x][y])
+            res += fi
+        liste.append(res)
+        res = ""
+    return retrouver(liste)
+
+app = Flask(__name__,  template_folder="template")#←,static_folder="static"
 
 @app.route('/')
 def indexer():
-	return render_template('user_choice.html')
+    return render_template('index.html')
 
-@app.route('/shipper_result', methods = ['POST'])
-def shipper_result():
-	try:
-		text_string = request.form.get('text_string')
-		if not isinstance(text_string, str) or not text_string.strip():
-			return render_template('page_error/error.html')
-		try:
-			private_key = int(request.form.get('private_key'))
-			public_key = int(request.form.get('public_key'))
-		except:
-			return render_template('page_error/error.html')
-		result = RSA_Protocol_decryptation(text_string, private_key, public_key)
-		return render_template('server_result.html', result = result)
-	except:
-		return render_template('page_error/error.html')
 
-@app.route('/recipient_result', methods = ['POST'])
-def recipient_result():
-	text_string = request.form.get('text_string')
-	if not isinstance(text_string, str) or not text_string.strip():
-		return render_template('page_error/error.html')
-	try:
-		public_key = int(request.form.get('public_key'))
-		partial_key = int(request.form.get('partial_key'))
-	except:
-		return render_template('page_error/error.html')
-	try:
-		result = RSA_Protocol_encryptation(text_string, public_key, partial_key)
-		return render_template('recipient_index.html', result = result)
-	except:
-		return render_template('page_error/error.html')
+@app.route('/TpyrKc4h', methods=["POST"])
+def TpyrKc4h():
+    texte = request.form.get('TEXTE')
+    key = request.form.get('cle')
+    resultat = chiffrerXor(texte, key)
+    #return f'{chiffrerXor(texte, key)}'
+    return render_template('result.html', message = resultat)
 
-@app.route('/recipient', methods = ['POST'])
-def recipient():
-	return render_template('recipient_index.html')
+@app.route('/en_d_crypt', methods=["POST"])
+def en_d_crypt():
+    return render_template('index.html')
 
-@app.route('/shipper', methods = ['POST'])
-def shipper():
-	return render_template('server_index.html')
 
-@app.route('/home', methods = ['POST'])
-def home():
-	return render_template('user_choice.html')
 
-@app.route('/generate_key', methods = ['POST'])
-def generate_key():
-	return render_template('key_generator.html')
-
-@app.route('/generating', methods = ['POST'])
-def generating():
-	report = key_generator()
-	return render_template('key_generator.html', private_key = report[0], public_key = report[1], partial_key = report[2])
-
-if __name__ == '__main__':
-	app.run(debug = True)
+if __name__ == "__main__":
+    app.run(debug = True)
